b39204b9a288c50afec2a83b1b91e187e2e40c28
Helga1970 <olga.kostrikina@gmail.com>
2025-08-25 20:20:23 +0200

    Update get-content.js

--- netlify/functions/get-content.js
+++ netlify/functions/get-content.js
@@ -25,6 +25,7 @@ const checkSubscription = async (email) => {
 
 exports.handler = async (event) => {
     const userEmail = event.headers['x-user-email'];
+    const page = event.queryStringParameters.page || 'menu';
 
     if (!userEmail) {
         return {
@@ -42,9 +43,37 @@ exports.handler = async (event) => {
         };
     }
 
+    const pages = {
+        'chitalnyizal': 'https://pro-culinaria.ru/chitalnyizal',
+        'new-book': 'https://pro-culinaria.ru/new-book'
+    };
+
+    if (page === 'menu') {
+        const menuHtml = `
+            <h2>Доступные материалы:</h2>
+            <ul>
+                <li><a href="#" onclick="loadContent('chitalnyizal'); return false;">Читальный зал</a></li>
+                <li><a href="#" onclick="loadContent('new-book'); return false;">Новая книга</a></li>
+            </ul>
+        `;
+        return {
+            statusCode: 200,
+            headers: { 'Content-Type': 'text/html' },
+            body: menuHtml
+        };
+    }
+
+    const pageUrl = pages[page];
+
+    if (!pageUrl) {
+        return {
+            statusCode: 404,
+            body: 'Страница не найдена.'
+        };
+    }
+
     try {
-        const tildaPageUrl = 'https://pro-culinaria.ru/chitalnyizal';
-        const response = await axios.get(tildaPageUrl, {
+        const response = await axios.get(pageUrl, {
             headers: {
                 'Referer': 'https://pro-culinaria.ru/'
             }
